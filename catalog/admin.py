7cd8081bec9e014014cc8c72c1082f78194aafaa
Visvaldis <sevoslav@gmail.com>
2019-12-07 21:20:54 +0200

    Added admin panel for products
    Signed-off-by: Visvaldis <sevoslav@gmail.com>

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
+
 
 # Register your models here.
+
+# Define the admin class
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+
+
+# Register the admin class with the associated model
+admin.site.register(Product, ProductAdmin)
+
+
+# Register the Admin classes for Category using the decorator
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
